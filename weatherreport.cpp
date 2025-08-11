32f08f170bee83d39c36964a8e087f663b81a626
Saigovula <129597706+Saigovula@users.noreply.github.com>
2025-08-11 17:34:46 +0530

    Update weatherreport.cpp

--- weatherreport.cpp
+++ weatherreport.cpp
@@ -1,15 +1,16 @@
 #include "weatherreport.h"
 #include <string.h>
 
-const char* Report(const struct WeatherSensor *sensor) {
-    int precipitation = sensor->Precipitation(sensor->impl);
-    static char report[64] = "Sunny Day";
+const char* Report(const struct WeatherSensor* sensor)
+{
+    int precipitation = sensor->Precipitation();
+    const char* report = "Sunny Day";
 
-    if (sensor->TemperatureInC(sensor->impl) > 25) {
+    if (sensor->TemperatureInC() > 25) {
         if (precipitation >= 20 && precipitation < 60) {
-            strcpy(report, "Partly Cloudy");
-        } else if (sensor->WindSpeedKMPH(sensor->impl) > 50) {
-            strcpy(report, "Alert, Stormy with heavy rain");
+            report = "Partly Cloudy";
+        } else if (sensor->WindSpeedKMPH() > 50) {
+            report = "Alert, Stormy with heavy rain";
         }
     }
     return report;
