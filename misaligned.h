3b76104a062bf8f7ac704c9add09c5b824899b6b
Saigovula <129597706+Saigovula@users.noreply.github.com>
2025-08-11 10:16:05 +0530

    Update misaligned.h

--- misaligned.h
+++ misaligned.h
@@ -1,2 +1,19 @@
-#include<functional>
-int printColorMap(std::function<void(std::string&)) ;
+#ifndef COLOR_MAP_H
+#define COLOR_MAP_H
+
+#define MAJOR_COLORS 5
+#define MINOR_COLORS 5
+
+struct ColorPair {
+    int index;
+    const char* major;
+    const char* minor;
+};
+
+// Function to generate the (buggy) color map
+void generateColorMap(struct ColorPair colorMap[]);
+
+// Generate the color map output as a single string
+void generateOutputString(char *buffer, size_t bufSize, struct ColorPair colorMap[]);
+
+#endif
